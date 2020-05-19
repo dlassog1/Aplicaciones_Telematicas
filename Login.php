6c49f8c039ddaea49b20b5104bafdfd24dc04efa
Drees69 <65146138+Drees69@users.noreply.github.com>
2020-05-19 16:54:41 -0500

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+        <title></title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Login</h1>
+        </div>
+        <div class="container">
+            <form>
+
+                <div class="form-group">
+                    <label for="exampleInputCorreo">Usuario</label>
+                    <input type="email" class="form-control" id="exampleInputCorreo" placeholder="Ingrese su usuario" name="txtusuariou">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputContraseñaU">Contraseña</label>
+                    <input type="password" class="form-control" id="exampleInputContraseñaU" placeholder="Ingrese la contraseña" name="txtpassu1">
+                </div>
+                
+                <button type="submit" class="btn btn-primary" name="btnaccederu">Acceder</button>
+                
+            </form>
+        </div>
+
+        <?php
+        // put your code here
+        ?>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+    </body>
+</html>
