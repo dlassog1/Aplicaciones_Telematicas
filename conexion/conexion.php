b98bfca2331ef62595a6781f3befcc23bde2b25b
dlassog1 <65137075+dlassog1@users.noreply.github.com>
2020-05-18 19:16:41 -0500

    Cambio 2
    Otro echo

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -6,7 +6,10 @@
        $conexion=new PDO("mysql:host=localhost;charset=UTF8;dbname=$dbname",$user,$pass);
        echo "Conexión Exitosa";
        echo "PRUEBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-        
+       echo "PRUEBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+       echo "PRUEBARA";
+       echo "PRUEBARA";
+       echo "PRUEBARA";
     } catch (Exception $ex) {
        echo "Conexion no exitosa";
     }
